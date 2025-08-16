13007cce1a637fb8d4737e7a9f3ac0ec82267a90
Biniyamgirma <119936137+Biniyamgirma@users.noreply.github.com>
2025-08-16 19:11:35 +0300

    added regular user functionality

--- controllers/regularUserController/regularUserController.js
+++ controllers/regularUserController/regularUserController.js
@@ -1,52 +1,49 @@
 const db= require("../../database/createDataBase");
 const bcrypt = require("bcrypt");
 
-const viewPostsInArea =  (req,res)=> {
+const viewPostsInArea = async (req,res)=> {
     const { townId } = req.body;
-    db.query("SELECT * FROM post WHERE area = ?", [area], (err, results) => {
-        if (err) {
+  const postInArea = await db.sql(`SELECT * FROM post WHERE area = ${area}`);
+        if (!postInArea) {
             console.error(err);
             return res.status(500).json({ error: "Internal server error" });
         }
-        return res.status(200).json(results);
-    });
+        return res.status(200).json(postInArea);
 
 }
-const viewPostDetails = (req, res) => {
+const viewPostDetails =async (req, res) => {
     const { postId } = req.body;
-    db.query("SELECT * FROM post WHERE id = ?", [postId], (err, results) => {
-        if (err) {
+const postDetails =await db.sql(`SELECT * FROM post WHERE id = ${postId}`);
+        if (!postDetails) {
             console.error(err);
             return res.status(500).json({ error: "Internal server error" });
         }
-        if (results.length === 0) {
+        if (postDetails.length === 0) {
             return res.status(404).json({ error: "Post not found" });
         }
-        return res.status(200).json(results[0]);
-    });
+        return res.status(200).json(postDetails[0]);
 }
-const viewUserProfile = (req, res) => {
+const viewUserProfile =async (req, res) => {
     const { userId } = req.body;
-    db.query("SELECT * FROM normalUser WHERE id = ?", [userId], (err, results) => {
-        if (err) {
+const normalUser=await db.sql(`SELECT * FROM normalUser WHERE id = ${userId}`);
+        if (!normalUser) {
             console.error(err);
             return res.status(500).json({ error: "Internal server error" });
         }
-        if (results.length === 0) {
+        if (normalUser.length === 0) {
             return res.status(404).json({ error: "User not found" });
         }
-        return res.status(200).json(results[0]);
-    });
+        return res.status(200).json(normalUser[0]);
 }
-const reportPost = (req, res) => {
+const reportPost =async (req, res) => {
     const { postId, reportDescription, userId, subCityId,townId,policeStationId } = req.body;
-    db.query("INSERT INTO report (postId, townId, subCityId, reportDescription, userId, policeStationId) VALUES (?, ?, ?,?,?,?)", [postId, townId, subCityId,reportDescription,userId,policeStationId], (err, results) => {
-        if (err) {
+const report=await db.sql(`INSERT INTO report (postId, townId, subCityId, reportDescription, userId, policeStationId) VALUES (${postId}, ${townId}, ${subCityId}, ${reportDescription}, ${userId}, ${policeStationId}))`);
+       
+if (!report) {
             console.error(err);
             return res.status(500).json({ error: "Internal server error" });
         }
         return res.status(201).json({ message: "Report submitted successfully" });
-    });
 }
 
 module.exports = {
