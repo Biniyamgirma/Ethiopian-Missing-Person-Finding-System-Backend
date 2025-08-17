8c77fec17ceee444c8e32ea78921cfaac095b5d8
Biniyamgirma <119936137+Biniyamgirma@users.noreply.github.com>
2025-08-17 21:07:41 +0300

    adjusted criminal route

--- routes/criminalsRoute/criminalsRoute.js
+++ routes/criminalsRoute/criminalsRoute.js
@@ -33,9 +33,9 @@ const {updateCriminal,
     getAllCriminals,
     addCriminal,
     deleteCriminal} = require("../../controllers/criminalsControllers/criminalsControllers");
-router.put("/updateCriminal/:criminalId",authMiddleware([1,2,3,4]), updateCriminal); // Assuming updateCriminal might also need multer if it handles file uploads
-router.get("/getAllCriminals",authMiddleware([1,2,3,4]), getAllCriminals);
-router.post("/addCriminal", upload,authMiddleware([1,2,3,4]), addCriminal); // Correctly apply 'upload' middleware here
-router.delete("/deleteCriminal/:criminalId",authMiddleware([1,2,3,4]), deleteCriminal);
+router.put("/updateCriminal/:criminalId", updateCriminal); // Assuming updateCriminal might also need multer if it handles file uploads
+router.get("/getAllCriminals", getAllCriminals);
+router.post("/addCriminal", upload, addCriminal); // Correctly apply 'upload' middleware here
+router.delete("/deleteCriminal/:criminalId", deleteCriminal);
 
 module.exports = router;
