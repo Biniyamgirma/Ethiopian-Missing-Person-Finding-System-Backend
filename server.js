86f626cdba68ae2c5db316ac722f038ff749e699
Biniyamgirma <119936137+Biniyamgirma@users.noreply.github.com>
2025-08-19 16:36:23 +0300

    added https://ethiopian-missing-person-sys.web.app/ url to cors config

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const path = require('path');
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use(cors({
-  origin:  'http://localhost:5173',
+  origin:  'https://ethiopian-missing-person-sys.web.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
